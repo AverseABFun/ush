2400685dbe8fb4333472008ef8985d4314f3f6bc
Arthur Beck <averse.abfun@gmail.com>
2024-01-05 15:57:26 +0000

    gwrnjigeijbij

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,40 @@ use std::env;
 use std::io::{stdin, stdout, Write};
 use std::path::Path;
 use std::process::{Child, Command, Stdio};
+extern crate clap;
+use clap::Parser;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(short = 'q', long = "quiet")]
+    quiet: bool
+}
 fn main() {
+    let args = Cli::parse();
+    if !args.quiet {
+        print!("Welcome to ush, the Update SHell\n");
+        print!("This is designed for use in changing hosted files, and originally created for use in making a collaboratively created ARG\n");
+    }
     loop {
-        // use the `>` character as the prompt
-        // need to explicitly flush this to ensure it prints before read_line
         print!("> ");
         stdout().flush().unwrap();
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        // read_line leaves a trailing newline, which trim removes
-        // this needs to be peekable so we can determine when we are on the last command
         let mut commands = input.trim().split(" | ").peekable();
         let mut previous_command = None;
 
         while let Some(command) = commands.next() {
-            // everything after the first whitespace character is interpreted as args to the command
+            
             let mut parts = command.trim().split_whitespace();
             let command = parts.next().unwrap();
             let args = parts;
 
             match command {
                 "cd" => {
-                    // default to '/' as new directory if one was not provided
+                    
                     let new_dir = args.peekable().peek().map_or("/", |x| *x);
                     let root = Path::new(new_dir);
                     if let Err(e) = env::set_current_dir(&root) {
@@ -42,12 +51,12 @@ fn main() {
                     });
 
                     let stdout = if commands.peek().is_some() {
-                        // there is another command piped behind this one
-                        // prepare to send output to the next command
+                        
+                        
                         Stdio::piped()
                     } else {
-                        // there are no more commands piped behind this one
-                        // send output to shell stdout
+                        
+                        
                         Stdio::inherit()
                     };
 
@@ -71,7 +80,7 @@ fn main() {
         }
 
         if let Some(mut final_command) = previous_command {
-            // block until the final command has finished
+            
             final_command.wait().unwrap();
         }
     }
